ece57cd7afa7756c7e4974c08a619f6f298941f7
arydeg <arydeg@gmail.com>
2017-12-21 21:25:10 -0300

    empezando por perfil

--- app/src/main/java/ar/edu/proyectosort/zapando/PerfilActivity.java
+++ app/src/main/java/ar/edu/proyectosort/zapando/PerfilActivity.java
@@ -3,13 +3,27 @@ package ar.edu.proyectosort.zapando;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class PerfilActivity extends AppCompatActivity {
+
+    TextView nom,ape,descrip,edad;
+    String nombre,apellido,descripcion,fecha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        nom = (TextView) findViewById(R.id.firstname);
+        ape = (TextView) findViewById(R.id.lastname);
+        descrip = (TextView) findViewById(R.id.description);
+        edad = (TextView) findViewById(R.id.birth);
+
+        nom.setText(nombre);
+        ape.setText(apellido);
+        descrip.setText(descripcion);
+        edad.setText(fecha);
 
     }
 
